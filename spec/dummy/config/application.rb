463d3524fc876ff5121a7ba8cf8cdf5962aa32fc
Thomas von Deyen <tvd@bitspire.de>
2016-05-18 21:18:14 +0200

    Upgrade dummy app to Rails 4.2.6

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -7,6 +7,8 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 require "alchemy/devise"
 
@@ -23,6 +25,8 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
-
