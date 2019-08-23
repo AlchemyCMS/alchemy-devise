ad1886d3d12cc43626ad4c8d50021c61400a47d7
Thomas von Deyen <thomas@vondeyen.com>
2019-08-23 10:08:04 +0200

    Split spec_helper and rails_helper

--- spec/features/security_feature_spec.rb
+++ spec/features/security_feature_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe "Security: " do
   context "If user is present" do
--- spec/requests/admin/users_controller_request_spec.rb
+++ spec/requests/admin/users_controller_request_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe Alchemy::Admin::UsersController, type: :request do
   before do
