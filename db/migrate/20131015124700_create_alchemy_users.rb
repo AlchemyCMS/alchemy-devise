90aca597e3fa565f94aa016d62434631b319c6c5
rmparr <ryan@ryanmparr.com>
2019-11-28 17:12:37 +0100

    Remove 'gender' column from existing migration and spec schema. This column is not being dropped from existing databases to preserve existing data.

--- db/migrate/20131015124700_create_alchemy_users.rb
+++ db/migrate/20131015124700_create_alchemy_users.rb
@@ -6,7 +6,6 @@ class CreateAlchemyUsers < ActiveRecord::Migration[4.2]
       t.string   "lastname"
       t.string   "login"
       t.string   "email"
-      t.string   "gender"
       t.string   "language"
       t.string   "encrypted_password",     limit: 128, default: "",       null: false
       t.string   "password_salt",          limit: 128, default: "",       null: false
