91d6b0e55a37304d93ef87110faf9434073755ee
Thomas von Deyen <thomas@vondeyen.com>
2020-08-07 11:41:29 +0200

    Autoformat alchemy initializer

--- config/initializers/alchemy.rb
+++ config/initializers/alchemy.rb
@@ -1,23 +1,23 @@
-require 'alchemy/devise/ability'
+require "alchemy/devise/ability"
 
 Alchemy.register_ability(Alchemy::Devise::Ability)
 
 Alchemy::Modules.register_module({
-  name: 'users',
-  engine_name: 'alchemy',
+  name: "users",
+  engine_name: "alchemy",
   position: 4.1,
   navigation: {
-    name: 'modules.users',
-    controller: '/alchemy/admin/users',
-    action: 'index',
-    icon: 'users'
-  }
+    name: "modules.users",
+    controller: "/alchemy/admin/users",
+    action: "index",
+    icon: "users",
+  },
 })
 
-Alchemy.user_class_name = 'Alchemy::User'
-Alchemy.signup_path     = '/admin/signup'
-Alchemy.login_path      = '/admin/login'
-Alchemy.logout_path     = '/admin/logout'
+Alchemy.user_class_name = "Alchemy::User"
+Alchemy.signup_path = "/admin/signup"
+Alchemy.login_path = "/admin/login"
+Alchemy.logout_path = "/admin/logout"
 
 if Alchemy.respond_to?(:logout_method)
   Rails.application.config.after_initialize do
